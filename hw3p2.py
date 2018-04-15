98f6e452d0e62f16e909991bf91fbddf76873e70
jgianoglio <jim@gianoglio.com>
2018-04-15 12:58:13 -0400

    minor changes

--- hw3p2.py
+++ hw3p2.py
@@ -43,44 +43,96 @@ def sample_gumbel(shape, eps=1e-10, out=None):
     U = out.resize_(shape).uniform_() if out is not None else torch.rand(shape)
     return - torch.log(eps - torch.log(U + eps))
 
+class WsjDataset(Dataset):
+    """
+    Dataset yields features, assignments, and labels
+    """
 
-class wsjModel(nn.Module):
+    def __init__(self, name, args, test=False):
+        super(WsjDataset, self).__init__()
+        self.name = name
+        data = np.load(os.path.join(args.data_directory, '{}.npy'.format(name)))
+        if test:
+            labels = [np.zeros((d[1].shape[0],), dtype=np.int32) for d in data]
+        else:
+            labels = np.load(os.path.join(args.data_directory, '{}-phonemes.npy'.format(name)))
+
+        # preprocessing
+        self.features = [torch.from_numpy(x[0].T) for x in data]
+        self.assignments = [torch.from_numpy(assignment_vector(x)).long() for x in data]
+        self.phonemes = [torch.from_numpy(y).long() for y in labels]
+        self.len = len(self.features)
+
+    def __getitem__(self, item):
+        return self.features[item], self.assignments[item], self.phonemes[item]
+
+    def __len__(self):
+        return self.len
+
+
+def wsj_collate_fn(batch):
+    """
+    Concatenate features, assignments and labels
+    """
+    padding = 40  # padding between utterances
+    # Count totals
+    n = len(batch)
+    frame_total = padding * 2 * n
+    phoneme_total = 0
+    for f, a, p in batch:
+        frame_total += f.size(1)
+        phoneme_total += p.size(0)
+    # Allocate storage
+    if _use_shared_memory:
+        frame_store = batch[0][0].storage()._new_shared(INPUT_DIM * frame_total)
+        collated_frames = batch[0][0].new(frame_store).resize_(1, INPUT_DIM, frame_total).zero_()
+        phoneme_store = batch[0][1].storage()._new_shared(phoneme_total)
+        collated_phonemes = batch[0][1].new(phoneme_store).resize_(phoneme_total).zero_()
+        assignment_store = batch[0][1].storage()._new_shared(frame_total)
+        collated_assignments = batch[0][1].new(assignment_store).resize_(frame_total).zero_()
+    else:
+        collated_frames = batch[0][0].new(1, INPUT_DIM, frame_total).zero_()
+        collated_phonemes = batch[0][1].new(phoneme_total).zero_()
+        collated_assignments = batch[0][1].new(frame_total).zero_()
+    # Collate
+    framepos = 0
+    phonemepos = 0
+    for f, a, p in batch:
+        startframe = framepos + padding
+        endframe = framepos + padding + f.size(1)
+        collated_frames[0, :, startframe:endframe] = f
+        collated_assignments[startframe:endframe] = a + phonemepos + 1
+        collated_phonemes[phonemepos:phonemepos + p.size(0)] = p
+        framepos += 2 * padding + f.size(1)
+        phonemepos += p.size(0)
+
+    # Return
+    return collated_frames, collated_assignments, collated_phonemes
+
+
+class WsjModel(nn.Module):
     def __init__(self, args):
-        super(wsjModel, self).__init__()
+        super(WsjModel, self).__init__()
         self.rnns = nn.ModuleList([
             nn.LSTM(input_size=args.embedding_dim, hidden_size=args.hidden_dim, batch_first=True),
             nn.LSTM(input_size=args.hidden_dim, hidden_size=args.hidden_dim, batch_first=True),
             nn.LSTM(input_size=args.hidden_dim, hidden_size=args.embedding_dim, batch_first=True)])
         self.projection = nn.Linear(in_features=args.embedding_dim, out_features=47)
 
-    def forward(self, input, forward=0, stochastic=False):
-        h = input  # (n, t)
-        h = self.embedding(h)  # (n, t, c)
-        states = []
-        for rnn in self.rnns:
-            h, state = rnn(h)
-            states.append(state)
-        h = self.projection(h)
-        if stochastic:
-            gumbel = Variable(sample_gumbel(shape=h.size(), out=h.data.new()))
-            h += gumbel
-        logits = h
-        if forward > 0:
-            outputs = []
-            h = torch.max(logits[:, -1:, :], dim=2)[1] + 1
-            for i in range(forward):
-                h = self.embedding(h)
-                for j, rnn in enumerate(self.rnns):
-                    h, state = rnn(h, states[j])
-                    states[j] = state
-                h = self.projection(h)
-                if stochastic:
-                    gumbel = Variable(sample_gumbel(shape=h.size(), out=h.data.new()))
-                    h += gumbel
-                outputs.append(h)
-                h = torch.max(h, dim=2)[1] + 1
-            logits = torch.cat([logits] + outputs, dim=1)
-        return logits
+    def forward(self, features):
+        # Model
+        h = features
+        for l in self.layers:
+            h = l(h)
+        raw_logits = torch.squeeze(h, 0)
+        # Pooling
+        oh = make_one_hot(assignments.data)
+        sum_logits = torch.mm(raw_logits, oh)  # (46, phonemes)
+        sum_assignment = torch.sum(oh, 0)
+        pooled_logits = sum_logits / sum_assignment
+        pooled_logits = torch.transpose(pooled_logits, 1, 0)  # (phonemes, 46)
+        # Return
+        return pooled_logits
 
 
 class CustomLogger(Callback):
@@ -99,58 +151,62 @@ def print_generated(lines):
         print("Generated text {}: {}".format(i, line))
 
 
-def train_model(model, dataset, args):
-    kw = {'num_workers': 2, 'pin_memory': True} if args.cuda else {}
-    loader = DataLoader(dataset=dataset, batch_size=args.batch_size, shuffle=True, **kw)
+def train_model(args):
+    """
+    Performs the training
+    """
+    if os.path.exists(os.path.join(args.save_directory, Trainer()._checkpoint_filename)):
+        # Skip training if checkpoint exists
+        return
+    model = WsjModel(args)
+    kwargs = {'num_workers': args.num_workers, 'pin_memory': True} if args.cuda else {}
+    train_loader = DataLoader(
+        WsjDataset('train', args), shuffle=True,
+        batch_size=args.batch_size, collate_fn=wsj_collate_fn, **kwargs)
+    validate_loader = DataLoader(
+        WsjDataset('dev', args), shuffle=True,
+        batch_size=args.batch_size, collate_fn=wsj_collate_fn, **kwargs)
+    # Build trainer
     trainer = Trainer(model) \
-        .build_criterion(CrossEntropyLoss3D) \
-        .build_metric(CategoricalError3D) \
-        .build_optimizer('Adam', weight_decay=1e-6) \
+        .build_criterion('CrossEntropyLoss', size_average=False) \
+        .build_metric('CategoricalError') \
+        .build_optimizer('Adam') \
+        .validate_every((1, 'epochs')) \
         .save_every((1, 'epochs')) \
         .save_to_directory(args.save_directory) \
         .set_max_num_epochs(args.epochs) \
-        .register_callback(CustomLogger) \
-        .bind_loader('train', loader)
+        .build_logger(TensorboardLogger(log_scalars_every=(1, 'iteration'),
+                                        log_images_every='never'),
+                      log_directory=args.save_directory)
 
+    # Bind loaders
+    trainer.bind_loader('train', train_loader, num_inputs=2)
+    trainer.bind_loader('validate', validate_loader, num_inputs=2)
+    trainer.register_callback(EpochTimer())
     if args.cuda:
         trainer.cuda()
 
     # Go!
     trainer.fit()
+    trainer.save()
 
 
 def main(argv):
-    # Argparse
-    parser = argparse.ArgumentParser(description='PyTorch Shakespeare Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--save-directory', type=str, default='output/shakespeare',
-                        help='output directory')
-    parser.add_argument('--epochs', type=int, default=20, metavar='N',
-                        help='number of epochs to train')
-    parser.add_argument('--batch-len', type=int, default=200, metavar='N',
-                        help='Batch length')
-    parser.add_argument('--hidden-dim', type=int, default=256, metavar='N',
-                        help='Hidden dim')
-    parser.add_argument('--embedding-dim', type=int, default=128, metavar='N',
-                        help='Embedding dim')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
+    # Training settings
+    parser = argparse.ArgumentParser(description='Homework 2 Part 2 Baseline')
+    parser.add_argument('--batch-size', type=int, default=32, metavar='N', help='input batch size')
+    parser.add_argument('--save-directory', type=str, default='output/simple/v1', help='output directory')
+    parser.add_argument('--data-directory', type=str, default='../', help='data directory')
+    parser.add_argument('--epochs', type=int, default=5, metavar='N', help='number of epochs to train')
+    parser.add_argument('--hidden', type=int, default=256, metavar='N', help='hidden units')
+    parser.add_argument('--num-workers', type=int, default=0, metavar='N', help='number of workers')
+    parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
+
     args = parser.parse_args(argv)
     args.cuda = not args.no_cuda and torch.cuda.is_available()
-    
 
-    # Train or load a model
-    checkpoint_path = os.path.join(args.save_directory, 'checkpoint.pytorch')
-    if not os.path.exists(checkpoint_path):
-        model = TextModel(charcount=charcount, args=args)
-        train_model(model=model, dataset=dataset, args=args)
-    else:
-        trainer = Trainer().load(from_directory=args.save_directory)
-        model = TextModel(charcount=charcount, args=args)
-        model.load_state_dict(trainer.model.state_dict())
-        if args.cuda:
-            model = model.cuda()
+    train_model(args)
+    write_predictions(args)
 
 
 if __name__ == '__main__':
